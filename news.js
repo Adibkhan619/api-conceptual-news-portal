00ab613220ee8034bb81c54ba736144d80827382
Adibkhan619 <work.adibkhan@gmail.com>
2024-03-02 12:36:59 +0600

    added loading spinner

--- news.js
+++ news.js
@@ -5,6 +5,7 @@ const loadCategory = async () => {
 
     const categoryContainer = document.getElementById('category-container-bar');
     data.data.news_category.forEach((item) => {
+
         const div = document.createElement("div");
 
         div.innerHTML = `<button onclick="loadNews('${item.category_id}')">${item.category_name}</button>`;
@@ -17,14 +18,22 @@ const loadCategory = async () => {
 loadCategory();
 
 const loadNews = async (catId) => {
+
+    document.getElementById('loading-spinner').style.display='block';
+
     const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${catId}`);
     const data = await response.json();
     console.log('singleNews:', data)
+
+    
 
     const singleNews = document.getElementById('single-news');
     singleNews.innerHTML = '';
 
     data.data.forEach((item) => {
+
+        document.getElementById('loading-spinner').style.display='none';
+
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="news-photo">
